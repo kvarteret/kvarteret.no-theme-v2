0aff223afa930bf54b438712fa2523a91b3c486f
Torstein Thune <torstein@gmail.com>
2012-10-11 16:05:47 +0200

    Added content-container to page-template

--- page.php
+++ page.php
@@ -13,17 +13,11 @@
  */
 
 get_header(); ?>
-
-	<div id="primary" class="site-content">
-		<div id="content" role="main">
-
+	<article id="primary" class="standard_wrapper content_container clearfix standard_padding">
 			<?php while ( have_posts() ) : the_post(); ?>
 				<?php get_template_part( 'content', 'page' ); ?>
 				<?php comments_template( '', true ); ?>
 			<?php endwhile; // end of the loop. ?>
-
-		</div><!-- #content -->
-	</div><!-- #primary -->
-
+	</article>
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>
