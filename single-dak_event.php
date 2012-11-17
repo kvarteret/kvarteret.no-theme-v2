41ed707bf28f54ec38dac6b0b908cc3e4ec0ed89
Torstein Thune <torstein@gmail.com>
2012-11-17 17:39:56 +0100

    Added template for single event

---
+++ single-dak_event.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * The Template for displaying all event posts.
+ *
+ * @package Kvarteret.no
+ * @subpackage Kvarteret.no v2
+ * @since Kvarteret.no v2.0
+ */
+
+/* TODO
+	Content, image, start-end, arranger, arranger logo, category
+*/
+
+get_header(); ?>
+
+	<article id="primary" class="standard_wrapper content_container clearfix">
+		<?php while ( have_posts() ) : the_post(); ?>
+		<header class="single_header article_header clearfix">
+			<?php 
+				if( has_post_thumbnail() ) {
+					the_post_thumbnail('full-thumbnail', array('class' => 'article_thumbnail responsive')); 
+				} else {
+					echo '<img src="' . get_bloginfo('template_directory') . '/images/missing_image.png" alt="" clasS="article_thumbnail responsive" />';
+				}
+			?>
+			<h1><?php the_title(); ?></h1>
+		</header>
+
+		<section class="article_content two-thirds column">
+			<?php the_content(); ?>
+		</section>
+
+		<aside class="meta one-third column">
+			<div class="date">
+				 <?php the_time( 'j. F Y' ); ?>
+			</div>
+			<?php echo kvarteret_author_meta(get_the_author_meta('ID'), get_post_meta($post->ID, "article_author", true)); ?> 
+			<div class="share clearfix">
+				<a href="https://www.facebook.com/sharer.php?u=<?php echo urlencode(get_permalink()); ?>&t=<?php echo urlencode(get_the_title()); ?>" class="facebook_share" title="Del på Facebook">Del på facebook</a>
+				<a href="http://twitter.com/share?url=<?php echo urlencode(get_permalink()); ?>&text=<?php the_title(); ?>&via=Kvarteret" target="_blank" rel="nofollow" class="twitter_share" alt="Del på Twitter">Del på Twitter</a>
+			</div>
+			<?php dynamic_sidebar('single-event'); ?>
+		</aside>
+		<?php endwhile; // end of the loop. ?>	
+
+
+	</article><!-- #primary -->
+	<script src="<?php bloginfo('template_directory'); ?>/js/jquery.kvarteret.socialMedia.js"></script>
+	<script type="text/javascript">
+		socialMedia('<?php echo urlencode(get_permalink()); ?>');
+		// socialMedia('http%3A%2F%2Fkvarteret.no')
+	</script>
+<?php get_footer(); ?>
